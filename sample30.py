a636ba5f833cb5a82d2913750a71740eff36b3de
kumarvadivel <35343907+kumarvadivel@users.noreply.github.com>
2020-02-19 11:36:14 +0530

    Add files via upload

---
+++ sample30.py
@@ -0,0 +1,9 @@
+#number conversions
+#to binary
+s1="{0:b}".format(16)
+print(s1)
+
+s1=16
+s1=bin(s1)
+print(s1)
+#prints with binary notation
---
+++ sample31.py
@@ -0,0 +1,6 @@
+#string alignment
+string="          {:^10}        {:^10}            {:^10}".format('hello','python','programmers')
+print(string)
+#rounding off integers
+s="{0:.2f}".format(1/6)
+print(s)
